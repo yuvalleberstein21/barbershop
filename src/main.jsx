7b030aff129d1ff8480ea9efa177e43aaccbedc2
MailBIT Sales <mailbitsales@MailBITs-MacBook-Pro.local>
2023-12-05 02:01:23 +0200

    update changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Spinner } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
 import { BrowserRouter } from 'react-router-dom';
+
+
+const LoadingScreen = () => {
+  return (
+    <div className='spinnerContainer'>
+      <div className='spinner'>
+        <Spinner size='xl' color='black' />
+      </div>
+    </div>
+
+  );
+};
+
+const MainApp = () => {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const delay = setTimeout(() => {
+      setLoading(false); // Set loading to false after a delay (e.g., 2000ms)
+    }, 2000);
+
+    return () => clearTimeout(delay);
+  }, []);
+
+  return isLoading ? <LoadingScreen /> : <App />;
+};
+
 
 
 const styles = {
@@ -29,7 +57,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-        <App />
+        <MainApp />
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
